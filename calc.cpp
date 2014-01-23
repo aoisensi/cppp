e063bb339e8adeda6dd33ba0032a8423f70b0172
aoisensi <aoisensi@aoisensi.info>
2014-01-24 07:19:12 +0900

    fufufu

--- calc.cpp
+++ calc.cpp
@@ -4,16 +4,29 @@
 
 namespace qi = boost::spirit::qi;
 
+template <typename Iterator>
+struct Formula : public qi::grammar<Iterator, qi::space_type> {
+	Formula() : Formula::base_type(expr) {
+		factor = qi::int_ | '(' >> expression >> ')';
+		expression = factor >> *('+' >> factor);
+		expr = expression >> qi::eoi;
+	}
+
+	qi::rule<Iterator, qi::space_type> factor;
+	qi::rule<Iterator, qi::space_type> expression;
+	qi::rule<Iterator, qi::space_type> expr;
+};
+
 int main(int argc, char *argv[]) {
-	if(argc == 1) return 1;
-	std::string s = argv[1];
-	std::cout << s << std::endl;
-	std::string::iterator first = s.begin(), last = s.end();
-	bool success = qi::parse(
-		first,
-		last,
-		qi::int_
-	);
-	if (success) { std::cout << "OK" << std::endl; }
-	return 0;
+	Formula<std::string::iterator> formula;
+	for(int i=1; i<argc;i++) {
+		std::string str = argv[i];
+		std::cout << str << std::endl;
+		std::string::iterator itrb = str.begin();
+		std::string::iterator itre = str.end();
+		if( qi::phrase_parse(itrb, itre, formula, qi::space))
+			std::cout << "OK" << std::endl;
+		else
+			std::cout << "NG" << std::endl;
+	}
 }
