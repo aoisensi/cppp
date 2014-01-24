b8f28cd13a5b6a5fecbaf7ded34769385ed6b5d8
aoisensi <aoisensi@aoisensi.info>
2014-01-24 09:07:06 +0900

    utreeeeeeeeeeee

--- calc.cpp
+++ calc.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <string>
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/support_utree.hpp>
 
 namespace qi = boost::spirit::qi;
+namespace spirit = boost::spirit;
 
 template <typename Iterator>
-struct Formula : public qi::grammar<Iterator, qi::space_type> {
+struct Formula : public qi::grammar<Iterator, spirit::utree()> {
 	Formula() : Formula::base_type(expr) {
-		factor = qi::int_ | '(' >> expression >> ')';
+		block = '(' >> expression >> ')';
+		factor = qi::int_ | block;
 		expression = factor >> *('+' >> factor);
 		expr = expression >> qi::eoi;
 	}
 
-	qi::rule<Iterator, qi::space_type> factor;
-	qi::rule<Iterator, qi::space_type> expression;
-	qi::rule<Iterator, qi::space_type> expr;
+	qi::rule<Iterator, spirit::utree::list_type()> block;
+	qi::rule<Iterator, spirit::utree()> factor;
+	qi::rule<Iterator, spirit::utree()> expression;
+	qi::rule<Iterator, spirit::utree()> expr;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 	Formula<std::string::iterator> formula;
 	for(int i=1; i<argc;i++) {
 		std::string str = argv[i];
 		std::cout << str << std::endl;
-		std::string::iterator itrb = str.begin();
-		std::string::iterator itre = str.end();
-		if( qi::phrase_parse(itrb, itre, formula, qi::space))
-			std::cout << "OK" << std::endl;
+		std::string::iterator itr = str.begin();
+		spirit::utree ut;
+		bool success = qi::parse(itr, str.end(), formula, ut);
+		if(success)
+			std::cout << ut << std::endl;
 		else
 			std::cout << "NG" << std::endl;
 	}
